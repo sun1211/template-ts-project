ae592d5bfcfbeb5de0d37430f419f7084bbd9e69
tam <ngoctam121191@gmail.com>
2022-05-18 15:30:11 +0700

    remove useless file

---
+++ src/app/components/NavBar/Nav.style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components/macro';
+import { StyleConstants } from 'styles/StyleConstants';
+
+export const Wrapper = styled.header`
+  box-shadow: 0 1px 0 0 ${p => p.theme.borderLight};
+  height: ${StyleConstants.NAV_BAR_HEIGHT};
+  display: flex;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: ${p => p.theme.background};
+  z-index: 2;
+
+  @supports (backdrop-filter: blur(10px)) {
+    backdrop-filter: blur(10px);
+    background-color: ${p =>
+      p.theme.background.replace(
+        /rgba?(\(\s*\d+\s*,\s*\d+\s*,\s*\d+)(?:\s*,.+?)?\)/,
+        'rgba$1,0.75)',
+      )};
+  }
+`;
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,9 +1,5 @@
-import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { NavBar } from 'app/components/NavBar';
-import { Masthead } from './Masthead';
-import { Features } from './Features';
-import { PageWrapper } from 'app/components/PageWrapper';
 
 export function HomePage() {
   return (
@@ -13,10 +9,6 @@ export function HomePage() {
         <meta name="description" content="A React Boilerplate application homepage" />
       </Helmet>
       <NavBar />
-      <PageWrapper>
-        <Masthead />
-        <Features />
-      </PageWrapper>
     </>
   );
 }
